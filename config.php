82179e75044238f049250aa51f1c4b312b0d181a
Meriem ELK <m.elkouarir@gmail.com>
2025-06-19 06:03:19 -0400

    tp1

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Chemin de base de l'application
-define('BASE', '/SESSION_3/WebAvancee/TP/TP2/locauto');
-
-// Chemin pour les assets (CSS, images)
-define('ASSET', '/SESSION_3/WebAvancee/TP/TP2/locauto/public/');
-?>
--- views/error.php
+++
@@ -1,8 +0,0 @@
-{{ include('layouts/header.php', {title: 'Error'})}}
- <div class="conatiner">
-      <div class="section_erreur">
-         <h2 >Error</h2>
-         <strong class="error">{{ message }}</strong>
-      </div>
- </div>
-{{ include('layouts/footer.php')}}
